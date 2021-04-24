0ca70f15571faa708b48bf8bab7933b5fb534f53
Noor El-Din Walid <noorwalid72@yahoo.com>
2021-04-24 21:31:37 +0200

    Initial Commit

---
+++ CrossWord/CrossWordApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrossWordApp.swift
+//  CrossWord
+//
+//  Created by Noor Walid on 24/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct CrossWordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
